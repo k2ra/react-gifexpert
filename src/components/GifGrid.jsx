13b70ee096c5a407f6612e3114c9f79f554fcd3d
kevin <kevinrojas.a30@gmail.com>
2022-09-01 11:46:59 -0500

    primer commit

---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,29 @@
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GridItem } from './GridItem';
+
+export const GifGrid = ({category}) => {
+    const { images, isLoading } = useFetchGifs(category);
+  return (
+    <>
+    <h3>{ category }</h3>
+    {
+        isLoading && (<h2>Cargando...</h2>)
+    }
+    <div className='card-grid'>
+        {
+            images.map( image =>(
+
+                <GridItem
+                    key={image.id} 
+                    title={image.title}
+                    url={image.url}
+                />
+            )
+            )
+        }
+
+    </div>
+    </>
+  )
+
+}
